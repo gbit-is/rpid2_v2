ae5b52508f638580b302de9a0b09de0f3c515c83
gbit-is <gbit@gbit.is>
2023-12-04 17:33:47 +0000

    further prereqs

--- raspberry_pi/setup/install_preReqs.sh
+++ raspberry_pi/setup/install_preReqs.sh
@@ -8,11 +8,20 @@ fi
 
 export script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# MQ
 sudo apt install beanstalkd -y 
 sudo systemctl enable beanstalkd
 sudo systemctl start beanstalkd
 
+# Apache
+sudo apt install apache2 -y
+sudo systemctl enable apache2 
+sudo systemctl start apache2
+sudo apt-get install libapache2-mod-wsgi-py3 -y
 
+
+
+#Python
 sudo apt install python3-venv -y
 
 cd $script_dir
@@ -20,3 +29,5 @@ cd ..
 python3 -m venv ./venv
 source ./venv/bin/activate
 pip install pystalk
+pip install flask
+pip install pygame
